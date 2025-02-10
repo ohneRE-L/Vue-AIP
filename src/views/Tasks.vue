b53e0aaac84b8932b54f89e828ce827aec271333
ohneRE-L <fazilov.p.i.23@gmail.com>
2025-02-10 09:01:00 +0300

    Задание 16

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -33,6 +33,27 @@
   <h1>|</h1>
   <h1>Задание 12</h1>
   <button @click="showCurrentDay">Показать текущий день недели</button>
+  <h1>|</h1>
+  <h1>Задание 13</h1>
+  <button @click="showSquare(2)">Квадрат 2</button>
+  <button @click="showSquare(3)">Квадрат 3</button>
+  <h1>|</h1>
+  <h1>Задание 14</h1>
+  <p>{{ text1 }}</p>
+  <button @click="changeText">Изменить текст</button>
+  <button @click="setText('Привет, Vue!')">Текст 1</button>
+  <button @click="setText('Hello, World!')">Текст 2</button>
+  <h1>|</h1>
+  <h1>Задание 15</h1>
+  <p>Цена за единицу: {{ cost }}</p>
+  <p>Количество: {{ amount }}</p>
+  <p>Общая стоимость: {{ price }}</p>
+  <button @click="changeCost">Изменить цену</button>
+  <h1>|</h1>
+  <h1>Задание 16</h1>
+  <a href="https://google.com" @click.prevent="handleClick">Не переходи по ссылке</a><br>
+  <button @click.once="handleClick2">Нажми меня один раз</button>
+
 </template>
 
 <script>
@@ -49,10 +70,36 @@ export default {
       num3: 3,
       arr: ['x', 'y', 'z'],
       arr1: [1, 2, 3],
-      obj: { x: 1, y: 2, z: 3 }
+      obj: { x: 1, y: 2, z: 3 },
+      text1: 'Начальный текст',
+      cost: 100,
+      amount: 5
+    }
+  },
+  computed: {
+    price() {
+      return this.cost * this.amount;
     }
   },
   methods: {
+    handleClick2() {
+      alert("Ты нажал кнопку, но больше не сможешь!");
+    },
+    handleClick() {
+      alert("Клик обработан, но переход заблокирован!");
+    },
+    changeCost() {
+      this.cost = Math.floor(Math.random() * 500) + 50; // Генерируем случайную цену
+    },
+    changeText() {
+      this.text1 = 'Текст изменен!';
+    },
+    setText(newText) {
+      this.text1 = newText;
+    },
+    showSquare(num) {
+      alert(num * num)
+    },
     showDate() {
       alert(new Date().toLocaleDateString());
     },
