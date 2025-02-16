037b2df55a43820e7eb7681e7227067a4347d641
ohneRE-L <fazilov.p.i.23@gmail.com>
2025-02-16 19:18:17 +0300

    Задание 39

--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -59,7 +59,7 @@
   <p v-else>Этот абзац отображается, если visible равно false</p>
   <h1>|</h1>
   <h1>Задание 18</h1>
-  <p v-if="!hidden">text</p>
+  <p v-if="!hidden18">text</p>
   <h1>|</h1>
   <h1>Задание 19.1</h1>
   <div>
@@ -134,8 +134,238 @@
   <h1>Задание 24</h1>
   <div>
     <p v-if="age24 < 18">подросток</p>
-    <p v-else-if="age24 >= 19 && age24 <= 25">молодой человек</p>
+    <p v-else-if="age24 <= 25">молодой человек</p>
     <p v-else>мужчина</p>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 25</h1>
+  <div>
+    <button @click="hideParagraphs25">Скрыть абзацы</button>
+    <template v-if="visible25">
+      <p>Абзац 1</p>
+      <p>Абзац 2</p>
+      <p>Абзац 3</p>
+    </template>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 26</h1>
+  <div>
+    <button @click="toggle26">Toggle абзаца</button>
+    <p v-show="visible26">Этот абзац отображается, если visible === true</p>
+  </div>
+  <!--Отличия директив v-show и v-if
+
+    v-if:
+        Полностью удаляет или создаёт элемент в DOM в зависимости от условия.
+        При изменении условия происходит создание или уничтожение дочерних компонентов и привязок событий.
+        Подходит для условий, которые редко меняются, поскольку операция создания/уничтожения может быть затратной.
+
+    v-show:
+        Элемент всегда присутствует в DOM, а управление его видимостью осуществляется через CSS (свойство display: none).
+        Переключение видимости происходит быстрее, так как DOM не пересоздаётся.
+        Не поддерживает конструкции v-else и не может применяться к тегу <template>.
+        №3: Когда использовать v-show, а когда v-if
+
+    v-if используется, если:
+        Условие изменяется нечасто.
+        Вам важно, чтобы при отсутствии условия элемент полностью не присутствовал в DOM (например, для экономии ресурсов или корректной работы дочерних компонентов).
+
+    v-show используется, если:
+        Переключения видимости происходят часто.
+        Элемент должен оставаться в DOM, и вам важна быстрота переключения, поскольку изменение свойства display происходит мгновенно.
+
+        Ограничения директивы v-show
+
+    Не поддерживает конструкцию v-else:
+    Вы не можете использовать v-show вместе с v-else или v-else-if, так как v-show просто меняет CSS-стиль, а не управляет созданием/уничтожением элементов.
+
+    Нельзя применять к элементу template:
+    Директива v-show должна применяться непосредственно к реальным DOM-элементам, а не к обёрткам типа <template>.
+
+    Всегда рендерит элемент:
+    Даже если элемент скрыт (display: none), он остаётся в DOM, что может быть нежелательно в случаях, когда необходимо полностью убрать элемент из структуры страницы для оптимизации.
+    -->
+  <h1>|</h1>
+  <h1>Задание 27.1</h1>
+  <div>
+    <div v-for="item in items271" :key="item">{{ item }}</div>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 27.2</h1>
+  <div>
+    <div v-for="item in items272" :key="item">{{ item * item }}</div>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 27.3</h1>
+  <ul>
+    <li v-for="item in items273" :key="item">{{ item }}</li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 28</h1>
+  <ul>
+    <li v-for="(elem, key) in arr28" :key="key">
+      {{ key }}
+    </li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 29.1</h1>
+  <ul>
+    <li v-for="(value, key) in obj291" :key="key">{{ value }}</li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 29.2</h1>
+  <ul>
+    <li v-for="(value, key) in obj292" :key="key">
+      {{ key }} - {{ value }}
+    </li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 29.3</h1>
+  <ul>
+    <li v-for="(value, key, index) in obj293" :key="key">
+      {{ key }} - {{ value }} - {{ index }}
+    </li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 29.4</h1>
+  <ul>
+    <li v-for="(value, key, index) in obj294" :key="key">
+      {{ key }} - {{ value }} - {{ index + 1 }}
+    </li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 30.1</h1>
+  <div v-for="item in items301" :key="item">
+    <p>{{ item }}</p>
+    <p class="divider"></p>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 30.2</h1>
+  <ul>
+    <template v-for="(item, index) in items302" :key="index">
+      <li>{{ item }}</li>
+      <li class="divider"></li>
+    </template>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 31.1</h1>
+  <ul>
+    <li v-for="(item, index) in hrefs311" :key="index">
+      <a :href="item.href">{{ item.text }}</a>
+    </li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 31.2</h1>
+  <table>
+    <tr v-for="(product, index) in products312" :key="index">
+      <td>{{ product.name }}</td>
+      <td>{{ product.price }}</td>
+      <td>{{ product.quantity }}</td>
+    </tr>
+  </table>
+  <h1>|</h1>
+  <h1>Задание 32</h1>
+  <div>
+    <p v-for="num in 30" :key="num">{{ num }}</p>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 33</h1>
+  <ul>
+    <template v-for="(elem, index) in items33" :key="index">
+      <li v-if="elem > 0">{{ elem }}</li>
+    </template>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 34</h1>
+  <ul>
+    <li v-for="product in products34" :key="product.id">
+      {{ product.name }}
+    </li>
+  </ul>
+  <h1>|</h1>
+  <h1>Задание 35.1</h1>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items351" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="addItem351">Добавить элемент</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 35.2</h1>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items352" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="removeFirstItem352">Удалить первый элемент</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 35.3</h1>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items353" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="removeLastItem353">Удалить последний элемент</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 35.4</h1>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items354" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="removePenultimateItem354">Удалить предпоследний элемент</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 35.5</h1>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items355" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="sortItems355">Сортировать элементы</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 35.6</h1>
+  <div>
+    <ul>
+      <li v-for="(item, index) in items356" :key="index">{{ item }}</li>
+    </ul>
+    <button @click="reverseItems356">Обратить порядок</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 36.1</h1>
+  <!-- Для задания 36.1 -->
+  <p class="task361">Первый абзац</p>
+
+  <!-- Для задания 36.2 -->
+  <p class="task362">Первый абзац</p>
+  <h1>|</h1>
+  <h1>Задание 36.2</h1>
+  <div>
+    <p362>Первый абзац</p362>
+    <br>
+    <p362>Второй абзац</p362>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 36.3</h1>
+  <div>
+    <p :style="{ color: textColor363 }">Динамический цвет текста</p>
+    <button @click="changeColor363">Изменить цвет</button>
+  </div>
+  <h1>|</h1>
+  <h1>Задание 37</h1>
+  <p :class="cssClasses371">Текст абзаца</p>
+  <p :class="classObject372">Текст абзаца</p>
+  <p :class="classArray373">Текст абзаца</p>
+  <p :class="[classObject374, 'static-class']">Текст абзаца</p>
+  <h1>|</h1>
+  <h1>Задание 38</h1>
+  <p :class="obj38">Текст с динамическими классами</p>
+  <h1>|</h1>
+  <h1>Задание 39</h1>
+  <div>
+    <p :class="obj39">Этот текст может быть скрыт или показан.</p>
+    <button @click="showElement39">Показать</button>
+    <button @click="hideElement39">Скрыть</button>
+    <button @click="toggleElement39">Тогглить</button>
   </div>
 </template>
 
@@ -143,6 +373,90 @@
 export default {
   data() {
     return {
+      obj39: {
+        hidden39: true,
+      },
+      obj38: {
+        done: true,
+        selected: false,
+      },
+      isActive374: true,
+      classArray373: ['class1', 'class2'],
+      isActive372: true,
+      isError372: false,
+      cssClasses371: 'class1 class2',
+      textColor363: 'green',
+      items356: ['Элемент 1', 'Элемент 2', 'Элемент 3'],
+      items355: ['Банан', 'Апельсин', 'Яблоко', 'Груша'],
+      items354: ['Элемент 1', 'Элемент 2', 'Элемент 3', 'Элемент 4'],
+      items353: ['Элемент 1', 'Элемент 2', 'Элемент 3'],
+      items352: ['Элемент 1', 'Элемент 2', 'Элемент 3'],
+      items351: ['Элемент 1', 'Элемент 2', 'Элемент 3'],
+      products34: [
+        {
+          id: 1,
+          name: 'product1',
+        },
+        {
+          id: 2,
+          name: 'product2',
+        },
+        {
+          id: 3,
+          name: 'product3',
+        },
+      ],
+      items33: [1, -2, 3, -4, 5],
+      products312: [
+        {
+          name: 'product1',
+          price: 100,
+          quantity: 5
+        },
+        {
+          name: 'product2',
+          price: 200,
+          quantity: 4
+        },
+        {
+          name: 'product3',
+          price: 300,
+          quantity: 3
+        }
+      ],
+      hrefs311: [
+        { href: '1.html', text: 'text1' },
+        { href: '2.html', text: 'text2' },
+        { href: '3.html', text: 'text3' }
+      ],
+      items302: [1, 2, 3],
+      items301: [1, 2, 3],
+      obj294: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$'
+      },
+      obj293: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$'
+      },
+      obj292: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$'
+      },
+      obj291: {
+        user1: '100$',
+        user2: '200$',
+        user3: '300$'
+      },
+      arr28: ['x', 'y', 'z'],
+      items273: [1, 2, 3, 4, 5],
+      items272: [1, 2, 3, 4, 5],
+      items271: [1, 2, 3, 4, 5],
+      visible26: true,
+      visible25: true,
       age24: 25,
       day23: 'Среда',
       isAdmin22: true,
@@ -155,6 +469,7 @@ export default {
       visible193: false,
       visible192: false,
       visible191: false,
+      hidden18: false,
       visible: true, // Измените на false, чтобы увидеть второй абзац
       name: 'john',
       surn: 'smit',
@@ -175,9 +490,59 @@ export default {
   computed: {
     price() {
       return this.cost * this.amount;
-    }
+    },
+    classObject372() {
+      return {
+        active: this.isActive372,
+        error: this.isError372
+      }
+    },
+    classObject374() {
+      return {
+        active: this.isActive374,
+        'text-large': !this.isActive374
+      }
+    },
   },
   methods: {
+    showElement39() {
+      this.obj39.hidden39 = false;
+    },
+    hideElement39() {
+      this.obj39.hidden39 = true;
+    },
+    toggleElement39() {
+      this.obj39.hidden39 = !this.obj39.hidden39;
+    },
+    changeColor363() {
+      this.textColor363 = this.textColor363 === 'green' ? 'purple' : 'green';
+    },
+    reverseItems356() {
+      this.items356.reverse();
+    },
+    sortItems355() {
+      this.items355.sort();
+    },
+    removePenultimateItem354() {
+      if (this.items354.length > 1) {
+        this.items354.splice(this.items354.length - 2, 1);
+      }
+    },
+    removeLastItem353() {
+      this.items353.pop();
+    },
+    removeFirstItem352() {
+      this.items352.shift();
+    },
+    addItem351() {
+      this.items351.push(`Элемент ${this.items351.length + 1}`);
+    },
+    toggle26() {
+      this.visible26 = !this.visible26;
+    },
+    hideParagraphs25() {
+      this.visible25 = false;
+    },
     toggle211() {
       this.visible211 = !this.visible211;
     },
@@ -256,3 +621,14 @@ export default {
 }
 </script>
 
+
+<style>
+.task361 { color: red; }
+.hidden39 {
+  display: none;
+}
+</style>
+
+<style scoped>
+.task362 { color: blue; }
+</style>
